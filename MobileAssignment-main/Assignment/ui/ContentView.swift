73642ed57a46e576b2b8d952cf4db1b85fc66c67
ios-sharma-deepak <ios.sharma.deepak@gmail.com>
2025-02-19 15:46:15 +0530

    Home page ui bug fix

---
+++ MobileAssignment-main/Assignment/ui/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Assignment
+//
+//  Created by Kunal on 03/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
+    @State private var path: [DeviceData] = [] // Navigation path
+
+    var body: some View {
+        NavigationStack(path: $path) {
+            Group {
+                if let computers = viewModel.data, !computers.isEmpty {
+                    DevicesList(devices: computers) { selectedComputer in
+                        viewModel.navigateToDetail(navigateDetail: selectedComputer)
+                    }
+                } else {
+                    ProgressView("Loading...")
+                }
+            }
+            .onChange(of: viewModel.navigateDetail, {
+                if let navigate = viewModel.navigateDetail{
+                    path.append(navigate)
+                }
+            })
+            .navigationTitle("Devices")
+            .navigationDestination(for: DeviceData.self) { computer in
+                DetailView(device: computer)
+            }
+            .onAppear {
+                viewModel.fetchAPI()
+//                let navigate = viewModel.navigateDetail
+//                if (viewModel.navigateDetail != nil) {
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        path.append(navigate!)
+//                    }
+//                }
+            }
+        }
+    }
+}
